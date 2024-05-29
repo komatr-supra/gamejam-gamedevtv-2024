924a7665e33a4735e7adecd96fbf3628f7163b7e
tewe <leventewe@gmail.com>
2024-05-30 00:41:04 +0200

    Off screen indicator fix

--- scenes/player scene/off_screen_indicator.gd
+++ scenes/player scene/off_screen_indicator.gd
@@ -1,18 +1,14 @@
 extends Node2D
 
 @onready var indicator: TextureRect = $Indicator
-const indicator_offset: Vector2 = Vector2(0, 0)
 var camera: Camera2D
 var on_screen = true
 var viewport_center: Vector2
-var border_offset: Vector2 = Vector2(40, 60)
-var max_indicator_position: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	camera = get_viewport().get_camera_2d()
 	viewport_center = Vector2(Vector2(get_viewport().size) / camera.zoom) / 2.0
-	max_indicator_position = viewport_center - border_offset
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,14 +16,8 @@ func _process(delta: float) -> void:
 		indicator.hide()
 	else:
 		indicator.show()
-		var local_to_camera = camera.to_local(global_position)
-		var indicator_position = Vector2(local_to_camera.x, local_to_camera.y)
-		if indicator_position.abs().aspect() > max_indicator_position.aspect():
-			indicator_position *= max_indicator_position.x / abs(indicator_position.x)
-		else:
-			indicator_position *= max_indicator_position.y / abs(indicator_position.y)
-		indicator.set_global_position(viewport_center + indicator_position - indicator_offset)
-
+		var indicator_position = camera.to_local(global_position)
+		indicator.set_global_position(viewport_center + indicator_position.clamp(Vector2(-1240, -680), Vector2(1240, 680)))
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	on_screen = false
