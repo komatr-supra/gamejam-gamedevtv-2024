8c6218315e651552426f3d3d78d48273b2d52834
fodss <114185396+fodss@users.noreply.github.com>
2024-06-01 02:53:38 +0200

    Added mute buttons to menu and pause scene

--- scenes/pause_scene/pause_scene.gd
+++ scenes/pause_scene/pause_scene.gd
@@ -1,5 +1,14 @@
 extends Control
 
+@onready var music_sprite = $MusicButton/AnimatedSprite2D
+@onready var sfx_sprite = $SFXButton/AnimatedSprite2D
+
+var music_mouse_in: bool = false
+var sfx_mouse_in: bool = false
+
+var music_bus = AudioServer.get_bus_index("Music")
+var sfx_bus = AudioServer.get_bus_index("SFX")
+
 func _ready():
 	self.hide()
 
@@ -10,3 +19,17 @@ func _on_resume_button_up():
 func _on_menu_button_up():
 	get_tree().paused = false
 	SceneTransition.change_scene_to_file("res://scenes/new start scene/menu.tscn")
+
+func _on_music_button_up():
+	AudioServer.set_bus_mute(music_bus, not AudioServer.is_bus_mute(music_bus))
+	if AudioServer.is_bus_mute(music_bus):
+		music_sprite.animation = "sound_off"
+	else:
+		music_sprite.animation = "sound_on"
+
+func _on_sfx_button_up():
+	AudioServer.set_bus_mute(sfx_bus, not AudioServer.is_bus_mute(sfx_bus))
+	if AudioServer.is_bus_mute(sfx_bus):
+		sfx_sprite.animation = "sound_off"
+	else:
+		sfx_sprite.animation = "sound_on"
